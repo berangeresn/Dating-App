31461107ff95c8132c58190ac808834913016f7d
berangeresn <webscribeo@gmail.com>
2020-02-25 21:55:47 +0100

    error handling

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -45,8 +45,6 @@ namespace DatingApp.API.Controllers
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
-            // return CreatedAtRoute()
-
         }
 
         [HttpPost("login")]
